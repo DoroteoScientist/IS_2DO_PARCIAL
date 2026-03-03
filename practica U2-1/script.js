6854db07d79a19215a2868a4c02b84d2bb9a775e
DoroteoScientist <trufitamagica@gmail.com>
2026-03-03 12:48:57 -0600

    Ya quedo

--- practica U2-1/script.js
+++ practica U2-1/script.js
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
+
 function cambiarColor() {
     document.body.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 };
-=======
-const integrantes = document.querySelectorAll('.integrante');
-const btnSiguiente = document.getElementById('btnSiguiente');
-let indiceActual = 0;
-
-integrantes[indiceActual].classList.add('active');
-btnSiguiente.addEventListener('click', () => {
-    integrantes[indiceActual].classList.remove('active');
-    indiceActual = (indiceActual + 1) % integrantes.length;
-    integrantes[indiceActual].classList.add('active');
-});
->>>>>>> c66a892d9292531f5d0c4c3840bb531c09da0e7f
